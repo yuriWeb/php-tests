67fc3134d4a9a3a525cec5595fa538f2991f14d4
yshanoylo <yuriy.shanoylo@amadeus.com>
2016-07-05 16:26:42 +0300

    DateTime improved

--- DateTime.php
+++ DateTime.php
@@ -1,12 +1,14 @@
 <?php
+define('DATE_FORMAT', '!Y.m.d');
 //define('DATE_FORMAT', 'Y.m.d');
-define('DATE_FORMAT', 'Y.m.d H:i');
+define('OUTPUT_DATE_FORMAT', 'Y.m.d H:i:s');
+//define('DATE_FORMAT', 'Y.m.d H:i');
 
 function convertToDate($dateString) {
     $date = DateTime::createFromFormat(DATE_FORMAT, $dateString);
 
     if ($date) {
-        echo $date->format(DATE_FORMAT) . '<br>';
+        echo $date->format(OUTPUT_DATE_FORMAT) . '<br>';
     } else {
         echo 'Date not parsed: ' . $dateString . '<br>';
     }
@@ -15,8 +17,8 @@ function convertToDate($dateString) {
 
 //convertToDate('2015.12.26');
 //convertToDate('20151226');
-convertToDate('2015.12.26 12:00');
-convertToDate('2015.12.26 12:0');
-convertToDate('2015.12.26 12');
-
-
+//convertToDate('2015.12.26 12:00');
+//convertToDate('2015.12.26 12:0');
+//convertToDate('2015.12.26 12');
+convertToDate('2015.12.26');
+//convertToDate(' ');
