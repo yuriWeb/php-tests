a98fdb36b76ea71f1623a5615b965f3486d7ab7e
yshanoylo <yuriy.shanoylo@amadeus.com>
2016-07-13 11:08:37 +0300

    closure test added

---
+++ closure.php
@@ -0,0 +1,52 @@
+<?php
+
+class Line {
+    public $visible = true;
+    public $text;
+
+    public function __construct($text, $visible = true) {
+        $this->text = $text;
+        $this->visible = $visible;
+    }
+}
+
+class TestClosure {
+    protected $lines = [];
+
+    public function __construct() {
+        $this->lines[] = new Line('first line');
+        $this->lines[] = new Line('second line', false);
+        $this->lines[] = new Line('third line');
+        $this->lines[] = new Line('fourth line');
+    }
+
+    public function printLines() {
+        $this->forAll(function(Line $line) {
+            echo $line->text . '<br>';
+        });
+    }
+
+    public function printVisibleLines($suffix) {
+        $this->forAllVisible(function(Line $line) use ($suffix) {
+            echo $line->text . $suffix . '<br>';
+        });
+    }
+
+    public function forAll(Closure $c) {
+        foreach ($this->lines as $line) {
+            $c($line);
+        }
+    }
+
+    public function forAllVisible(Closure $c) {
+        /** @var Line $line */
+        foreach ($this->lines as $line) {
+            if ($line->visible) {
+                $c($line);
+            }
+        }
+    }
+}
+
+$test = new TestClosure();
+$test->printVisibleLines('_suf');
