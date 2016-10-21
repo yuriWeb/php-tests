b8f328da8d68b2ea8507467969efdb928100a7e2
yshanoylo <yuriy.shanoylo@amadeus.com>
2016-10-21 14:29:13 +0300

    MultipleIterator and trait tests changed

--- MultipleIterator.php
+++ MultipleIterator.php
@@ -1,8 +1,9 @@
 <?php
+//TODO: it is interesting to test memory usage  for big arrays
 $people = new ArrayIterator(array('John', 'Jane', 'Jack', 'Judy'));
 $roles  = new ArrayIterator(array('Developer', 'Scrum Master', 'Project Owner'));
 
-$team = new MultipleIterator();
+$team = new MultipleIterator(MultipleIterator::MIT_KEYS_ASSOC);
 $team->attachIterator($people, 'person');
 $team->attachIterator($roles, 'role');
 
--- trait.php
+++ trait.php
@@ -1,4 +1,37 @@
 <?php
+
+Trait Tester
+{
+    protected function test1()
+    {
+        echo 'test1';
+    }
+
+    protected function test2()
+    {
+        echo 'test2';
+    }
+}
+
+class Test
+{
+    use Tester {
+        test1 as public;
+    }
+
+    public function test3() {
+        $this->test2();
+        echo 'test3';
+    }
+}
+
+$test = new Test();
+$test->test1();
+$test->test3();
+$test->test2();
+
+
+/*
 class A
 {
     public function foo()
@@ -38,3 +71,4 @@ class B extends A
 $instance = new B();
 $instance->foo();
 $instance->bar();
+*/
